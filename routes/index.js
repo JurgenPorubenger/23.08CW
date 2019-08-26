ede844339c3aa62521ffe8eaf752cab65d8e8294
jurgen <jurgenporubenger@gmail.com>
2019-08-26 15:36:02 +0300

    last

--- routes/index.js
+++ routes/index.js
@@ -12,27 +12,37 @@ let main = { articles: [
             text: 'LoremLorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.',
             autor: function () {
                return usr.names[0].name
-            }},
+            },
+            id:1
+        },
         { artTitle: 'BlaBlaBla2',
             text: 'Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.',
             autor: function () {
                 return usr.names[1].name
-            }},
+            },
+            id:2
+        },
         { artTitle: 'BlaBlaBla3',
             text: 'В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов.',
             autor: function () {
                 return usr.names[2].name
-            }},
+            },
+            id:3
+        },
         { artTitle: 'BlaBlaBla4',
             text: 'Используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно',
             autor: function () {
                 return usr.names[3].name
-            }},
+            },
+            id:4
+        },
         { artTitle: 'BlaBlaBla5',
             text: 'Используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно',
             autor: function () {
                 return usr.names[4].name
-            }},
+            },
+            id:5
+        },
     ]
     };
 
@@ -41,10 +51,11 @@ let main = { articles: [
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Blog Page', articles: main.articles });
 });
-router.get('/:id', function(req, res, next) {
+router.get('/blog/:id', function(req, res, next) {
     main.articles.forEach((item,i)=>{
         if(main.articles[i]['id']==req.params.id){
-            res.render('index', { title: artTitle, articles: main.articles });
+            // console.log(req.params.id+" "+ main.articles.text)
+            res.render('blogPage', { title: main.articles[i].artTitle, text: main.articles[i].text, autor: main.articles[i].autor(), id: main.articles[i].id});
         }
     })
 });
